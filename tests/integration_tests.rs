98d889b91d435f1712499d3f5e15dc75fd8b9e44
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2018-06-20 12:57:11 +0100

    test/integration: add basic tests incorporating faulty nodes

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -25,12 +25,13 @@
     unused_qualifications, unused_results
 )]
 #![allow(
-    box_pointers, missing_copy_implementations, missing_debug_implementations, unused,
+    box_pointers, missing_copy_implementations, missing_debug_implementations,
     variant_size_differences
 )]
 
 #[macro_use]
 extern crate lazy_static;
+extern crate maidsafe_utilities;
 extern crate parsec;
 extern crate rand;
 extern crate rust_sodium;
@@ -41,45 +42,169 @@ extern crate unwrap;
 
 mod utils;
 
-use self::utils::{init_rng, FullId, Network, Transaction};
-use parsec::SecretId;
+use self::utils::{Network, Transaction};
+use rand::Rng;
 use std::collections::BTreeSet;
 
 #[ignore]
 #[test]
 fn minimal_network() {
-    let mut rng = init_rng(None);
+    let mut rng = utils::init_rng(None);
 
     // 4 is the minimal network size for which the super majority is less than it.
     let num_peers = 4;
-    let max_iterations = 100;
 
     let mut network = Network::new(num_peers);
-    let id = FullId::new();
-    let transaction = Transaction::InsertPeer(*id.public_id());
+    let transaction = rng.gen();
 
-    for peer in network.peers.values_mut() {
-        assert!(!peer.have_voted_for(&transaction));
-        assert!(peer.vote_for(transaction.clone()).is_ok());
+    for peer in &mut network.peers {
+        assert!(!peer.parsec.have_voted_for(&transaction));
+        unwrap!(peer.parsec.vote_for(transaction.clone()));
     }
 
-    let mut consensued_peer = BTreeSet::new();
-    let mut iterations = 0;
-    loop {
-        assert!(iterations < max_iterations);
+    let mut consensued_peers = BTreeSet::new();
+    utils::loop_with_max_iterations(100, || {
         network.send_random_syncs(&mut rng);
-        for (peer_id, peer) in &mut network.peers {
-            if peer.poll().is_some() {
-                let _ = consensued_peer.insert(*peer_id);
+        for peer in &mut network.peers {
+            if peer.parsec.poll().is_some() {
+                let _ = consensued_peers.insert(peer.id);
             }
         }
-        if consensued_peer.len() == num_peers {
-            break;
+        consensued_peers.len() == num_peers
+    });
+
+    for peer in &network.peers {
+        assert!(peer.parsec.have_voted_for(&transaction));
+    }
+}
+
+#[ignore]
+#[test]
+fn faulty_third_never_gossip() {
+    let mut rng = utils::init_rng(None);
+    let num_peers = 10;
+    let num_faulty = (num_peers - 1) / 3;
+    let num_transactions = 10;
+
+    let mut network = Network::new(num_peers);
+    let mut transactions = (0..num_transactions)
+        .map(|_| rng.gen())
+        .collect::<Vec<Transaction>>();
+
+    // Remove faulty peers from `network`.
+    rng.shuffle(&mut network.peers);
+    network.peers.truncate(num_peers - num_faulty);
+
+    // Have each remaining peer vote for all transactions in random order.
+    for peer in &mut network.peers {
+        rng.shuffle(&mut transactions);
+        for transaction in &transactions {
+            unwrap!(peer.parsec.vote_for(transaction.clone()));
         }
-        iterations += 1;
     }
 
-    for peer in network.peers.values() {
-        assert!(peer.have_voted_for(&transaction));
+    // Gossip to let all remaining peers reach consensus on all the blocks.
+    utils::loop_with_max_iterations(100, || {
+        network.send_random_syncs(&mut rng);
+        for peer in &mut network.peers {
+            peer.poll();
+        }
+        network
+            .peers
+            .iter()
+            .all(|peer| peer.blocks.len() >= num_transactions)
+    });
+
+    assert!(network.blocks_all_in_sequence());
+}
+
+#[ignore]
+#[test]
+fn faulty_third_terminate_concurrently() {
+    let mut rng = utils::init_rng(None);
+    let num_peers = 10;
+    let num_faulty = (num_peers - 1) / 3;
+    let num_transactions = 10;
+
+    let mut network = Network::new(num_peers);
+    let mut transactions = (0..num_transactions)
+        .map(|_| rng.gen())
+        .collect::<Vec<Transaction>>();
+
+    // Have each peer vote for all transactions in random order.
+    for peer in &mut network.peers {
+        rng.shuffle(&mut transactions);
+        for transaction in &transactions {
+            unwrap!(peer.parsec.vote_for(transaction.clone()));
+        }
     }
+
+    // While gossiping, at a single random point remove all faulty peers in one go.
+    utils::loop_with_max_iterations(100, || {
+        network.send_random_syncs(&mut rng);
+        for peer in &mut network.peers {
+            peer.poll();
+        }
+
+        if network.peers.len() > num_peers - num_faulty
+            && (rng.gen_weighted_bool(10)
+                || network
+                    .peers
+                    .iter()
+                    .any(|peer| peer.blocks.len() >= num_transactions / 2))
+        {
+            rng.shuffle(&mut network.peers);
+            network.peers.truncate(num_peers - num_faulty);
+        }
+
+        network
+            .peers
+            .iter()
+            .all(|peer| peer.blocks.len() >= num_transactions)
+    });
+
+    assert!(network.blocks_all_in_sequence());
+}
+
+#[ignore]
+#[test]
+fn faulty_third_terminate_at_random_points() {
+    let mut rng = utils::init_rng(None);
+    let num_peers = 10;
+    let num_faulty = (num_peers - 1) / 3;
+    let num_transactions = 10;
+
+    let mut network = Network::new(num_peers);
+    let mut transactions = (0..num_transactions)
+        .map(|_| rng.gen())
+        .collect::<Vec<Transaction>>();
+
+    // Have each peer vote for all transactions in random order.
+    for peer in &mut network.peers {
+        rng.shuffle(&mut transactions);
+        for transaction in &transactions {
+            unwrap!(peer.parsec.vote_for(transaction.clone()));
+        }
+    }
+
+    // While gossiping, at random points remove a single faulty peer, up to a maximum of
+    // `num_faulty` peers removed in total.
+    utils::loop_with_max_iterations(100, || {
+        network.send_random_syncs(&mut rng);
+        for peer in &mut network.peers {
+            peer.poll();
+        }
+
+        if network.peers.len() > num_peers - num_faulty && rng.gen_weighted_bool(3) {
+            rng.shuffle(&mut network.peers);
+            let _ = network.peers.pop();
+        }
+
+        network
+            .peers
+            .iter()
+            .all(|peer| peer.blocks.len() >= num_transactions)
+    });
+
+    assert!(network.blocks_all_in_sequence());
 }
