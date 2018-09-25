0fe338852c51a85bc09d60f1d9ca5cd8a71e3696
Adam Cigánek <adam.ciganek@gmail.com>
2018-09-25 09:57:06 +0200

    refactor/tests: clean up integration tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -6,6 +6,7 @@
 // KIND, either express or implied. Please review the Licences for the specific language governing
 // permissions and limitations relating to use of the SAFE Network Software.
 
+#![cfg(feature = "testing")]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/maidsafe/QA/master/Images/maidsafe_logo.png",
     html_favicon_url = "https://maidsafe.net/img/favicon.ico",
@@ -54,222 +55,215 @@
     variant_size_differences
 )]
 
-#[cfg(feature = "testing")]
 extern crate maidsafe_utilities;
-#[cfg(feature = "testing")]
 extern crate parsec;
 #[macro_use]
-#[cfg(feature = "testing")]
 extern crate proptest;
-#[cfg(feature = "testing")]
 extern crate rand;
 
-#[cfg(feature = "testing")]
-mod test {
-    use maidsafe_utilities::log;
-    use parsec::dev_utils::proptest::{arbitrary_delay, ScheduleOptionsStrategy, ScheduleStrategy};
-    use parsec::dev_utils::{DelayDistribution, Environment, RngChoice, Schedule, ScheduleOptions};
-    use proptest::prelude::ProptestConfig;
-    use proptest::test_runner::FileFailurePersistence;
-    use rand::Rng;
-    use std::collections::BTreeMap;
+use maidsafe_utilities::log;
+use parsec::dev_utils::proptest::{arbitrary_delay, ScheduleOptionsStrategy, ScheduleStrategy};
+use parsec::dev_utils::{DelayDistribution, Environment, RngChoice, Schedule, ScheduleOptions};
+use proptest::prelude::ProptestConfig;
+use proptest::test_runner::FileFailurePersistence;
+use rand::Rng;
+use std::collections::BTreeMap;
 
-    // Alter the seed here to reproduce failures
-    static SEED: RngChoice = RngChoice::SeededRandom;
+// Alter the seed here to reproduce failures
+static SEED: RngChoice = RngChoice::SeededRandom;
 
-    #[test]
-    fn minimal_network() {
-        // 4 is the minimal network size for which the super majority is less than it.
-        let num_peers = 4;
-        let mut env = Environment::new(SEED);
+#[test]
+fn minimal_network() {
+    // 4 is the minimal network size for which the super majority is less than it.
+    let num_peers = 4;
+    let mut env = Environment::new(SEED);
 
-        let schedule = Schedule::new(
-            &mut env,
-            &ScheduleOptions {
-                genesis_size: num_peers,
-                opaque_to_add: 1,
-                votes_before_gossip: true,
-                ..Default::default()
-            },
-        );
-
-        let result = env.network.execute_schedule(schedule);
-        assert!(result.is_ok(), "{:?}", result);
-    }
-
-    #[test]
-    fn multiple_votes_before_gossip() {
-        let num_observations = 10;
-        let mut env = Environment::new(SEED);
-
-        let schedule = Schedule::new(
-            &mut env,
-            &ScheduleOptions {
-                opaque_to_add: num_observations,
-                votes_before_gossip: true,
-                ..Default::default()
-            },
-        );
-
-        let result = env.network.execute_schedule(schedule);
-        assert!(result.is_ok(), "{:?}", result);
-    }
-
-    #[test]
-    fn multiple_votes_during_gossip() {
-        let num_observations = 10;
-        let mut env = Environment::new(SEED);
-
-        let schedule = Schedule::new(
-            &mut env,
-            &ScheduleOptions {
-                opaque_to_add: num_observations,
-                ..Default::default()
-            },
-        );
-
-        let result = env.network.execute_schedule(schedule);
-        assert!(result.is_ok(), "{:?}", result);
-    }
-
-    #[test]
-    fn duplicate_votes_before_gossip() {
-        let mut env = Environment::new(SEED);
-
-        let schedule = Schedule::new(
-            &mut env,
-            &ScheduleOptions {
-                votes_before_gossip: true,
-                prob_vote_duplication: 0.5,
-                ..Default::default()
-            },
-        );
-
-        let result = env.network.execute_schedule(schedule);
-        assert!(result.is_ok(), "{:?}", result);
-    }
-
-    #[test]
-    fn faulty_third_never_gossip() {
-        let num_peers = 10;
-        let num_observations = 10;
-        let num_faulty = (num_peers - 1) / 3;
-        let mut env = Environment::new(SEED);
-
-        let mut failures = BTreeMap::new();
-        let _ = failures.insert(0, num_faulty);
-        let schedule = Schedule::new(
-            &mut env,
-            &ScheduleOptions {
-                genesis_size: num_peers,
-                opaque_to_add: num_observations,
-                deterministic_failures: failures,
-                ..Default::default()
-            },
-        );
-
-        let result = env.network.execute_schedule(schedule);
-        assert!(result.is_ok(), "{:?}", result);
-    }
-
-    #[test]
-    fn faulty_third_terminate_concurrently() {
-        let num_peers = 10;
-        let num_observations = 10;
-        let num_faulty = (num_peers - 1) / 3;
-        let mut env = Environment::new(SEED);
-
-        let mut failures = BTreeMap::new();
-        let _ = failures.insert(env.rng.gen_range(10, 50), num_faulty);
-        let schedule = Schedule::new(
-            &mut env,
-            &ScheduleOptions {
-                genesis_size: num_peers,
-                opaque_to_add: num_observations,
-                deterministic_failures: failures,
-                ..Default::default()
-            },
-        );
-
-        let result = env.network.execute_schedule(schedule);
-        assert!(result.is_ok(), "{:?}", result);
-    }
-
-    #[test]
-    fn faulty_nodes_terminate_at_random_points() {
-        let num_peers = 10;
-        let num_observations = 10;
-        let prob_failure = 0.05;
-        let mut env = Environment::new(SEED);
-        let schedule = Schedule::new(
-            &mut env,
-            &ScheduleOptions {
-                genesis_size: num_peers,
-                opaque_to_add: num_observations,
-                prob_failure,
-                ..Default::default()
-            },
-        );
-
-        let result = env.network.execute_schedule(schedule);
-        assert!(result.is_ok(), "{:?}", result);
-    }
-
-    #[test]
-    fn random_schedule_no_delays() {
-        let num_observations = 10;
-        let mut env = Environment::new(SEED);
-        let schedule = Schedule::new(
-            &mut env,
-            &ScheduleOptions {
-                opaque_to_add: num_observations,
-                delay_distr: DelayDistribution::Constant(0),
-                ..Default::default()
-            },
-        );
-
-        let result = env.network.execute_schedule(schedule);
-        assert!(result.is_ok(), "{:?}", result);
-    }
-
-    #[test]
-    fn add_peers() {
-        let mut env = Environment::new(SEED);
-        let schedule = Schedule::new(
-            &mut env,
-            &ScheduleOptions {
-                peers_to_add: 2,
-                ..Default::default()
-            },
-        );
-
-        let result = env.network.execute_schedule(schedule);
-        assert!(result.is_ok(), "{:?}", result);
-    }
-
-    proptest! {
-        #![proptest_config(ProptestConfig {
-            failure_persistence: Some(Box::new(FileFailurePersistence::WithSource("regressions"))),
-            cases: 5,
+    let schedule = Schedule::new(
+        &mut env,
+        &ScheduleOptions {
+            genesis_size: num_peers,
+            opaque_to_add: 1,
+            votes_before_gossip: true,
             ..Default::default()
-        })]
+        },
+    );
 
-        #[test]
-        fn agreement_under_various_conditions((mut env, sched) in ScheduleStrategy {
-            opts: ScheduleOptionsStrategy {
-                num_peers: (4..=10).into(),
-                num_observations: (1..=10).into(),
-                local_step: (0.01..=1.0).into(),
-                recv_trans: (0.001..0.5).into(),
-                failure: (0.0..1.0).into(),
-                vote_duplication: (0.0..0.5).into(),
-                delay_distr: arbitrary_delay(0..10, 0.0..10.0),
-            },
-        }) {
-            let _ = log::init(true);
+    let result = env.network.execute_schedule(schedule);
+    assert!(result.is_ok(), "{:?}", result);
+}
 
-            let result = env.network.execute_schedule(sched);
-            assert!(result.is_ok(), "{:?}", result);
-        }
+#[test]
+fn multiple_votes_before_gossip() {
+    let num_observations = 10;
+    let mut env = Environment::new(SEED);
+
+    let schedule = Schedule::new(
+        &mut env,
+        &ScheduleOptions {
+            opaque_to_add: num_observations,
+            votes_before_gossip: true,
+            ..Default::default()
+        },
+    );
+
+    let result = env.network.execute_schedule(schedule);
+    assert!(result.is_ok(), "{:?}", result);
+}
+
+#[test]
+fn multiple_votes_during_gossip() {
+    let num_observations = 10;
+    let mut env = Environment::new(SEED);
+
+    let schedule = Schedule::new(
+        &mut env,
+        &ScheduleOptions {
+            opaque_to_add: num_observations,
+            ..Default::default()
+        },
+    );
+
+    let result = env.network.execute_schedule(schedule);
+    assert!(result.is_ok(), "{:?}", result);
+}
+
+#[test]
+fn duplicate_votes_before_gossip() {
+    let mut env = Environment::new(SEED);
+
+    let schedule = Schedule::new(
+        &mut env,
+        &ScheduleOptions {
+            votes_before_gossip: true,
+            prob_vote_duplication: 0.5,
+            ..Default::default()
+        },
+    );
+
+    let result = env.network.execute_schedule(schedule);
+    assert!(result.is_ok(), "{:?}", result);
+}
+
+#[test]
+fn faulty_third_never_gossip() {
+    let num_peers = 10;
+    let num_observations = 10;
+    let num_faulty = (num_peers - 1) / 3;
+    let mut env = Environment::new(SEED);
+
+    let mut failures = BTreeMap::new();
+    let _ = failures.insert(0, num_faulty);
+    let schedule = Schedule::new(
+        &mut env,
+        &ScheduleOptions {
+            genesis_size: num_peers,
+            opaque_to_add: num_observations,
+            deterministic_failures: failures,
+            ..Default::default()
+        },
+    );
+
+    let result = env.network.execute_schedule(schedule);
+    assert!(result.is_ok(), "{:?}", result);
+}
+
+#[test]
+fn faulty_third_terminate_concurrently() {
+    let num_peers = 10;
+    let num_observations = 10;
+    let num_faulty = (num_peers - 1) / 3;
+    let mut env = Environment::new(SEED);
+
+    let mut failures = BTreeMap::new();
+    let _ = failures.insert(env.rng.gen_range(10, 50), num_faulty);
+    let schedule = Schedule::new(
+        &mut env,
+        &ScheduleOptions {
+            genesis_size: num_peers,
+            opaque_to_add: num_observations,
+            deterministic_failures: failures,
+            ..Default::default()
+        },
+    );
+
+    let result = env.network.execute_schedule(schedule);
+    assert!(result.is_ok(), "{:?}", result);
+}
+
+#[test]
+fn faulty_nodes_terminate_at_random_points() {
+    let num_peers = 10;
+    let num_observations = 10;
+    let prob_failure = 0.05;
+    let mut env = Environment::new(SEED);
+    let schedule = Schedule::new(
+        &mut env,
+        &ScheduleOptions {
+            genesis_size: num_peers,
+            opaque_to_add: num_observations,
+            prob_failure,
+            ..Default::default()
+        },
+    );
+
+    let result = env.network.execute_schedule(schedule);
+    assert!(result.is_ok(), "{:?}", result);
+}
+
+#[test]
+fn random_schedule_no_delays() {
+    let num_observations = 10;
+    let mut env = Environment::new(SEED);
+    let schedule = Schedule::new(
+        &mut env,
+        &ScheduleOptions {
+            opaque_to_add: num_observations,
+            delay_distr: DelayDistribution::Constant(0),
+            ..Default::default()
+        },
+    );
+
+    let result = env.network.execute_schedule(schedule);
+    assert!(result.is_ok(), "{:?}", result);
+}
+
+#[test]
+fn add_peers() {
+    let mut env = Environment::new(SEED);
+    let schedule = Schedule::new(
+        &mut env,
+        &ScheduleOptions {
+            peers_to_add: 2,
+            ..Default::default()
+        },
+    );
+
+    let result = env.network.execute_schedule(schedule);
+    assert!(result.is_ok(), "{:?}", result);
+}
+
+proptest! {
+    #![proptest_config(ProptestConfig {
+        failure_persistence: Some(Box::new(FileFailurePersistence::WithSource("regressions"))),
+        cases: 5,
+        ..Default::default()
+    })]
+
+    #[test]
+    fn agreement_under_various_conditions((mut env, sched) in ScheduleStrategy {
+        opts: ScheduleOptionsStrategy {
+            num_peers: (4..=10).into(),
+            num_observations: (1..=10).into(),
+            local_step: (0.01..=1.0).into(),
+            recv_trans: (0.001..0.5).into(),
+            failure: (0.0..1.0).into(),
+            vote_duplication: (0.0..0.5).into(),
+            delay_distr: arbitrary_delay(0..10, 0.0..10.0),
+        },
+    }) {
+        let _ = log::init(true);
+
+        let result = env.network.execute_schedule(sched);
+        assert!(result.is_ok(), "{:?}", result);
     }
 }
